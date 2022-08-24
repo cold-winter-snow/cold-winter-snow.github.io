528c18447524314dfc5aa0363a0a77af1d512157
Winter Snow <unknown@si.deep>
2022-08-24 22:49:17 +0300

    Add things

--- src/jsMain/kotlin/Registry.kt
+++ src/jsMain/kotlin/Registry.kt
@@ -78,7 +78,7 @@ private inline fun contentInfo(block: ContentInfoBuilder.() -> Unit): ContentInf
     ContentInfoBuilder().apply(block).build()
 
 // Assign production data instead of test one
-private val data by lazy { testData }
+private val data by lazy { realData }
 
 private val testData = listOf(
     contentInfo {
@@ -109,6 +109,201 @@ private val testData = listOf(
         title = "Failed to load data"
         access = ContentAccess.Common
         name = "eesdfyguhijklkjb"
+        format = ContentFormat.TXT
+    }
+)
+
+private val realData = listOf(
+    contentInfo {
+        title = "Храм"
+        name = "1"
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        hideTitle = true
+        title = "Machine"
+        name = "2"
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        hideTitle = true
+        name = "3"
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        hideTitle = true
+        title = "Путешествие"
+        name = "4"
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        hideTitle = true
+        name = "5"
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        hideTitle = true
+        name = "6"
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        hideTitle = true
+        title = "Страны, города"
+        name = "7"
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        hideTitle = true
+        name = "8"
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        hideTitle = true
+        name = "9"
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        hideTitle = true
+        title = "Ад Хаос Пустота"
+        name = "10"
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        hideTitle = true
+        name = "Было сердце одно"
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        hideTitle = true
+        name = "в никуда"
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        hideTitle = true
+        name = "В синем небе"
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        hideTitle = true
+        name = "В сто тысяч первый раз"
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        hideTitle = true
+        name = "В тот вечер растворились звёзды"
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        hideTitle = true
+        name = "В тумане пустынных видений"
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        hideTitle = true
+        name = "Выход"
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        hideTitle = true
+        name = "Где"
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        hideTitle = true
+        name = "Дождь"
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        hideTitle = true
+        name = "другой мир"
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        hideTitle = true
+        name = "ждать"
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        hideTitle = true
+        name = "Зачем писать заметки в телефоне"
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        hideTitle = true
+        name = "Крик"
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        hideTitle = true
+        name = "Меч"
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        name = "Молитвы неизвестному"
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        name = "На дальний берег"
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        name = "Обычно"
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        name = "Одинокий монумент"
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        name = "Он кидал сердце в небо"
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        name = "Он смотрел на закат Вселенной"
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        name = "Поэзия"
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        name = "Сказки уходят"
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        name = "Слова..."
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        name = "Смерти путь"
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        name = "Строки"
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        name = "Зачем писать заметки в телефоне"
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        name = "Только лишь вслед"
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        name = "Ты забыл"
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        name = "Ты неисправим"
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        name = "Это же прошлое"
+        format = ContentFormat.TXT
+    },
+    contentInfo {
+        name = "Я бы"
         format = ContentFormat.TXT
     }
 )
